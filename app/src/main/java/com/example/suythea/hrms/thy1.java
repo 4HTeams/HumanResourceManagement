87f7c4e86ee6d1e8f8d7120859e8d6aa0b6dc76e
lolzzlolzz <phengsengvuthy@gmail.com>
2016-10-12 20:54:54 +0700

    Added Thy1

---
+++ app/src/main/java/com/example/suythea/hrms/thy1.java
@@ -0,0 +1,13 @@
+package com.example.suythea.hrms;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class thy1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thy1);
+    }
+}
