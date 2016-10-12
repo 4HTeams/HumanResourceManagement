b957407c1ed2ee4e6bf8036ab2d0f434f0bf473d
raa-jr <chhaysotheara11@gmail.com>
2016-10-12 22:09:34 +0700

    GOGO2

--- app/src/main/java/com/example/suythea/hrms/Suythea.java
+++ app/src/main/java/com/example/suythea/hrms/Suythea.java
@@ -9,5 +9,6 @@ public class Suythea extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_suythea);
+
     }
 }
