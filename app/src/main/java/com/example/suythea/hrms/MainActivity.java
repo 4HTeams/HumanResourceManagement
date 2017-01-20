eef414e2c9eff794e49b7d21c41500138e98d9b3
lolzzlolzz <phengsengvuthy@gmail.com>
2017-01-20 14:14:44 +0700

    Created TabBar Pager !

--- app/src/main/java/com/example/suythea/hrms/MainActivity.java
+++ app/src/main/java/com/example/suythea/hrms/MainActivity.java
@@ -1,13 +1,54 @@
 package com.example.suythea.hrms;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.example.suythea.hrms.Home.MainHome;
+import com.example.suythea.hrms.Setting.MainSetting;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    ViewPagerAdapter viewPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setControls();
+        setEvents();
+        startUp();
     }
+
+    void setControls() {
+        toolbar = (Toolbar) findViewById(R.id.toolBar);
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+    }
+
+    void setEvents(){
+
+    }
+
+    void startUp(){
+        setTitle("Welcome To HRMS !");
+        setSupportActionBar(toolbar);
+
+        // To Add SubFragments
+        viewPagerAdapter.addFragmentAndTitle(new MainHome(), "Home");
+        viewPagerAdapter.addFragmentAndTitle(new MainSetting(), "Setting");
+
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+
+
 }
