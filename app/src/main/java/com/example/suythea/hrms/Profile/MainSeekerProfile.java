f66822e6c1ae9ad9a8b0f3988306e7c297534a14
Suythea <suytheaseng555@gmail.com>
2017-02-21 16:28:29 +0700

    prepare folder

---
+++ app/src/main/java/com/example/suythea/hrms/Profile/MainSeekerProfile.java
@@ -0,0 +1,31 @@
+package com.example.suythea.hrms.Profile;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.suythea.hrms.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainSeekerProfile extends Fragment {
+
+
+    public MainSeekerProfile() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main_seeker_profile, container, false);
+    }
+
+}
