a47a69a89d0975dc9b6d2480c11ce624f7140deb
lolzzlolzz <phengsengvuthy@gmail.com>
2017-03-26 09:53:59 +0700

    Sorry I am really tired to describe !
    Been working on upgrading to company
    Completed upgrading to company without conditions

--- app/src/main/java/com/example/suythea/hrms/Account/MainEditUpgradeToCom.java
+++ app/src/main/java/com/example/suythea/hrms/Account/MainEditUpgradeToCom.java
@@ -14,6 +14,7 @@ import android.widget.EditText;
 import android.widget.Spinner;
 
 import com.example.suythea.hrms.Interfaces.MySupporter_Interface;
+import com.example.suythea.hrms.Interfaces.Setting_Interface;
 import com.example.suythea.hrms.R;
 import com.example.suythea.hrms.Setting.MainSetting;
 import com.example.suythea.hrms.Supporting_Files.MySqlite;
@@ -40,6 +41,7 @@ public class MainEditUpgradeToCom extends AppCompatActivity implements MySupport
     Spinner spiIndustry, spiCType;
     Boolean existingInDB = false;
     EditText eTCName, eTCEmail, eTCEmpAmount, eTCContact, eTCAbout, eTCAddress;
+    Map<String, String> params;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -178,7 +180,7 @@ public class MainEditUpgradeToCom extends AppCompatActivity implements MySupport
                 e.printStackTrace();
             }
 
-            Map<String, String> params = new HashMap<>();
+            params = new HashMap<>();
             params.put("appToken","ThEa331RA369RiTH383thY925");
             params.put("id",sqlite.getDataFromjsonField(MySqlite.fields.get(0),"id"));
             params.put("cName",_eTCName);
@@ -192,7 +194,9 @@ public class MainEditUpgradeToCom extends AppCompatActivity implements MySupport
             params.put("orderToDo",order);
             params.put("conPassword",sqlite.getDataFromjsonField(MySqlite.fields.get(0),"password"));
 
+            MySupporter.Http("http://bongnu.khmerlabs.com/bongnu/account/edit_com.php", params, this);
             Log.d("result", String.valueOf(params));
+            MySupporter.showLoading("Please wait.....");
 
         } catch (JSONException e) {
             e.printStackTrace();
@@ -208,14 +212,67 @@ public class MainEditUpgradeToCom extends AppCompatActivity implements MySupport
         MySupporter.Volley("http://bongnu.khmerlabs.com/bongnu/get_data_tbl.php", params, (MySupporter_Interface) this);
     }
 
+    private void alterJson (String response){
+
+        try {
+
+            MySqlite sqlite = new MySqlite(this);
+            JSONObject oldData = new JSONArray(sqlite.getDataFromjsonField(MySqlite.fields.get(0),"_all_db")).getJSONObject(0);
+
+            JSONObject jsonObject = new JSONObject();
+            JSONObject jsonComInfo = new JSONObject();
+
+            String industry = String.valueOf(spiIndustry.getSelectedItem());
+            String cType = String.valueOf(spiCType.getSelectedItem());
+            String cID = new JSONArray(response).getJSONObject(0).getString("cid");
+
+            jsonObject.put("id", oldData.getString("id"));
+            jsonObject.put("username", oldData.getString("username"));
+            jsonObject.put("password", oldData.getString("password"));
+            jsonObject.put("email", oldData.getString("email"));
+            jsonObject.put("profile_url", oldData.getString("profile_url"));
+            jsonObject.put("type", "2");
+            jsonObject.put("approval", oldData.getString("approval"));
+
+            jsonComInfo.put("cID", cID);
+            jsonComInfo.put("cName", params.get("cName"));
+            jsonComInfo.put("cEmail", params.get("cEmail"));
+            jsonComInfo.put("industry", industry);
+            jsonComInfo.put("cType", cType);
+            jsonComInfo.put("empAmount", params.get("empAmount"));
+            jsonComInfo.put("address", params.get("address"));
+            jsonComInfo.put("contact", params.get("contact"));
+            jsonComInfo.put("about", params.get("about"));
+
+            jsonObject.put("companyInfo", jsonComInfo);
+
+            String finalUserData = "[" + String.valueOf(jsonObject) + "]";
+
+            sqlite.insertJsonDB(MySqlite.fields.get(0), finalUserData);
+
+            // Change to Company Profile Fragment
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        MySupporter.hideLoading();
+
+        Setting_Interface setting_interface = MainSetting.context;
+        setting_interface.changeToFragment("COMPANY_PROFILE");
+
+        finish();
+    }
+
+
     @Override
     public void onHttpFinished(String response) {
-
+        alterJson (response);
     }
 
     @Override
     public void onHttpError(String message) {
-
+        MySupporter.hideLoading();
     }
 
     @Override
